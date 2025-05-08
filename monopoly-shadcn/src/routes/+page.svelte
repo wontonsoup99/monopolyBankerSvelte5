b05ca258f074de29edef1d62d3f77e8059d4ec3f
wontonsoup99 <chase.powell456@gmail.com>
2025-05-07 20:36:56 -0600

    completed layout and styling for the menu dropdown

--- monopoly-shadcn/src/routes/+page.svelte
+++ monopoly-shadcn/src/routes/+page.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
-	import * as Card from '$lib/components/ui/card/index.js';
-	import { TriangleAlert } from 'lucide-svelte';
-	import * as Select from '$lib/components/ui/select/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { buttonVariants } from "$lib/components/ui/button/index.js";
+	import { Menu } from 'lucide-svelte';
+	import { CreditCard } from 'lucide-svelte';
+	import { RotateCw } from 'lucide-svelte';
+	import { CircleX} from 'lucide-svelte';
+	import { Users } from 'lucide-svelte';
+	import { Newspaper } from 'lucide-svelte';
 </script>
 
-<Card.Root class=" flex-1 gap-4 p-4 w-[320px]  rounded-lg">
-		<h3 class="text-lg font-semibold">Remove Player?</h3>
-		<div class="pt-4 pb-4">
-		<p class="pb-1 font-normal text-gray-700">Player</p>
-		<Select.Root type="single" name="player-select">
-			<Select.Trigger>Select player</Select.Trigger>
-			<Select.Content>
-				<Select.Item value="player1">Player 1</Select.Item>
-				<Select.Item value="player2">Player 2</Select.Item>
-			</Select.Content>
-		</Select.Root>
-	</div>
-	<Card.Content class="flex-1 align-top justify-star p-0">
-		<h3>Are you sure you want to remove [player] from the game?</h3>
-		<br>
-		<h3>It is okay if they are winning!</h3>
-	</Card.Content>
-	<Card.Footer class="flex-1 align-top gap-2 p-0 pt-4">
-		<Button variant="outline" class="flex-1 justify-center p-4 text-base font-semibold pt-1 pb-1">Cancel</Button>
-		<Button variant="destructive" class="flex-1 justify-center p-4 text-base font-semibold pt-1 pb-1 ">Remove</Button>
 
-	</Card.Footer>
-</Card.Root>
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger class={`${buttonVariants({ variant: "outline" })} size-10 flex-1 justify-center align-middle p-0`}><Menu class="size-6"/></DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+	  <DropdownMenu.Group>
+		<DropdownMenu.Item><Users />Add Player</DropdownMenu.Item>
+		<DropdownMenu.Item><CreditCard />Set Starting Money</DropdownMenu.Item>
+		<DropdownMenu.Item><RotateCw />Restart Game</DropdownMenu.Item>
+		<DropdownMenu.Item><CircleX />Remove Player</DropdownMenu.Item>
+		<DropdownMenu.Separator />
+		<DropdownMenu.Item><Newspaper />Privacy Policy</DropdownMenu.Item>
+		<DropdownMenu.Item><Newspaper />Terms and Conditions</DropdownMenu.Item>
+	  </DropdownMenu.Group>
+	</DropdownMenu.Content>
+  </DropdownMenu.Root>
