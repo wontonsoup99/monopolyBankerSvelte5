aad5bb2c73231943b8b34a7d61dfd260f8162dc0
wontonsoup99 <chase.powell456@gmail.com>
2025-05-07 21:10:03 -0600

    added functionality to the remove player component that dynamically updates the dropdown list with playernames

--- monopoly-shadcn/src/lib/components/popups/removePlayerPopup/removePlayerPopup.svelte
+++ monopoly-shadcn/src/lib/components/popups/removePlayerPopup/removePlayerPopup.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import { TriangleAlert } from 'lucide-svelte';
 	import * as Select from '$lib/components/ui/select/index.js';
+
+	//temporary test array to test if dropdown functionality is working
+	let players = [
+    { value: 'player1', label: 'Player One' },
+    { value: 'player2', label: 'Player Two' },
+    { value: 'player3', label: 'Player Three' }
+  ];
+  let value = $state("");
+	const triggerContent = $derived(
+	 players.find((f) => f.value === value)?.label ?? "Select a player"
+	);
 </script>
+
 
 <Card.Root class=" flex-1 gap-4 p-4 w-[320px]  rounded-lg">
 		<h3 class="text-lg font-semibold">Remove Player?</h3>
 		<div class="pt-4 pb-4">
 		<p class="pb-1 font-normal text-gray-700">Player</p>
-		<Select.Root type="single" name="player-select">
-			<Select.Trigger>Select player</Select.Trigger>
-			<Select.Content>
-				<Select.Item value="player1">Player 1</Select.Item>
-				<Select.Item value="player2">Player 2</Select.Item>
-			</Select.Content>
-		</Select.Root>
+		   <Select.Root type="single" name="Select Player" bind:value>
+	<Select.Trigger class="w-[180px]">
+	 {triggerContent}
+	</Select.Trigger>
+	<Select.Content>
+	 <Select.Group>
+	  <Select.GroupHeading>Select a Player</Select.GroupHeading>
+	  {#each players as player (player.value)}
+	   <Select.Item value={player.value} label={player.label}
+		>{player.label}</Select.Item
+	   >
+	  {/each}
+	 </Select.Group>
+	</Select.Content>
+   </Select.Root>
 	</div>
 	<Card.Content class="flex-1 align-top justify-star p-0">
 		<h3>Are you sure you want to remove [player] from the game?</h3>
--- monopoly-shadcn/src/routes/test/+page.svelte
+++ monopoly-shadcn/src/routes/test/+page.svelte
@@ -3,9 +3,11 @@
     import AddPlayerPopup from '$lib/components/popups/addPlayerPopup/addPlayerPopup.svelte';
 	import RestartGamePopup from '$lib/components/popups/restartGamePopup/restartGamePopup.svelte';
 	import RemovePlayerPopup from '$lib/components/popups/removePlayerPopup/removePlayerPopup.svelte';
+	import MenuDropdown from '$lib/components/menuDropdown/menuDropdown.svelte';
 </script>
 
 <AddPlayerPopup />
 <SetStartMoneyPopup />
 <RestartGamePopup />
 <RemovePlayerPopup />
+<MenuDropdown />
