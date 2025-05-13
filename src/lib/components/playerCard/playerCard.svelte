07eaf4d1b51263950893ad6fb5e68d3cf61bacb2
wontonsoup99 <chase.powell456@gmail.com>
2025-05-12 21:57:35 -0600

    added array of icon paths to the player card component

--- src/lib/components/playerCard/playerCard.svelte
+++ src/lib/components/playerCard/playerCard.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
+	let monopolyIconPath = [
+		'/Monopoly-Hat.png',
+		'/Monopoly-Car.png',
+		'/Monopoly-Cannon.png',
+		'/Monopoly-Ship.png',
+		'Monopoly-Shoe.png',
+		'Monopoly-Wheelbarrow.png'
+	];
 </script>
 
-<Card.Root class="w-[103px] h-[144px] justify-center align-top gap-1 flex-1 p-2">
-		<img src='/img-placeholder.png' alt='placeholder' class=" size-20"/>
-		<h2 class="text-center">$1,500</h2>
-		<h3 class="text-center">[Name]</h3>
+<Card.Root class="h-[144px] w-[103px] flex-1 justify-center gap-1 p-2 align-top">
+	<img src={monopolyIconPath[1]} alt="placeholder" class=" size-20" />
+	<h2 class="text-center">$1,500</h2>
+	<h3 class="text-center">[Name]</h3>
 </Card.Root>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import SetStartMoneyPopup from '$lib/components/popups/setStartMoneyPopup/setStartMoneyPopup.svelte';
-    import AddPlayerPopup from '$lib/components/popups/addPlayerPopup/addPlayerPopup.svelte';
+	import AddPlayerPopup from '$lib/components/popups/addPlayerPopup/addPlayerPopup.svelte';
 	import RestartGamePopup from '$lib/components/popups/restartGamePopup/restartGamePopup.svelte';
 	import RemovePlayerPopup from '$lib/components/popups/removePlayerPopup/removePlayerPopup.svelte';
 	import MenuDropdown from '$lib/components/menuDropdown/menuDropdown.svelte';
+	import PlayerCard from '$lib/components/playerCard/playerCard.svelte';
 </script>
 
 <AddPlayerPopup />
@@ -11,3 +12,4 @@
 <RestartGamePopup />
 <RemovePlayerPopup />
 <MenuDropdown />
+<PlayerCard />
