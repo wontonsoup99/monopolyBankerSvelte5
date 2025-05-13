56ce64c36f9a46b23918adf29c22e1ccca75d348
wontonsoup99 <chase.powell456@gmail.com>
2025-05-12 23:17:05 -0600

    added capacitor to projectinstalled ios, and android platforms, changed configs for capacitor, created a build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.monopolybanker.app',
+	appName: 'monopoly banker',
+	webDir: 'build'
+};
+
+export default config;
